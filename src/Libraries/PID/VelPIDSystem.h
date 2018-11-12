137e98d43b9704d6ffac1854ba77aee114a0a15a
theol0403 <16546293+theol0403@users.noreply.github.com>
2018-11-11 16:38:28 -0800

    imma do PID system

---
+++ src/Libraries/PID/VelPIDSystem.h
@@ -0,0 +1,22 @@
+#pragma once
+
+struct pidStruct
+{
+	float Kp;
+	float Ki;
+	float Kd;
+	float Kf;
+
+	int integralCap;
+	int integralInner;
+	int integralOuter;
+
+	int Error;
+  int totalError;
+	int lastError;
+	int lastTime;
+	int lastIntegral;
+  float derivative;
+
+  EMAFilter raName;
+};
