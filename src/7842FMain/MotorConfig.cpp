fdb395553297c9aa0cfb0b15a8d4f69912f99416
theol0403 <theol0403@gmail.com>
2019-01-28 17:06:26 -0800

    I'm using a t e s t robot

--- src/7842FMain/MotorConfig.cpp
+++ src/7842FMain/MotorConfig.cpp
@@ -26,6 +26,43 @@ lib7842::OdomController* chassis = nullptr;
 lib7842::AutonSelector *autonSelector = nullptr;
 
 
+void setFlywheelPower(double speed)
+{
+	m_Flywheel->moveVoltage(speed/127.0*12000);
+	m_Flywheel2->moveVoltage(speed/127.0*12000);
+}
+double getFlywheelRPM() { return m_Flywheel->getActualVelocity() * 15; } // 1:15 ratio from motor output to flywhel speed
+
+void setIntakePower(double speed) { m_Intake->moveVoltage(speed/127.0*12000); }
+void setIndexerPower(double speed) { m_Indexer->moveVoltage(speed/127.0*12000); }
+void setIntakeVelocity(double speed) { m_Intake->moveVelocity(speed); }
+void setIndexerVelocity(double speed) {	m_Indexer->moveVelocity(speed); }
+
+double getIndexerSensor() {	return s_indexerSensor->get_value_calibrated(); }
+
+
+void checkBaseStatus()
+{
+	if(model == nullptr)
+	{
+		std::cout << "USING BASE BEFORE INIT\n";
+		pros::delay(500);
+	}
+}
+
+void setBaseArcade(double yPower, double zPower)
+{
+	checkBaseStatus();
+	model->arcade(yPower, zPower, 0);
+}
+
+void setBasePower(double leftPower, double rightPower)
+{
+	checkBaseStatus();
+	model->tank(leftPower, rightPower, 0);
+}
+
+
 /***
 *     _____                            _   _ _   _              ______      _           _
 *    /  __ \                          | | (_) | (_)             | ___ \    | |         | |
@@ -103,29 +140,11 @@ void initializeBase()
 		);
 
 
-		// // Otherwise, you should specify the gearset and scales for your robot
-		// model = ChassisControllerFactory::createPtr(
-		// 	{e_m_LeftFront, e_m_LeftBack}, {e_m_RightFront, e_m_RightBack},
-		// 	*s_leftEncoder, *s_rightEncoder,
-		// 	IterativePosPIDController::Gains{0.001, 0.00, 0},
-		// 	IterativePosPIDController::Gains{0.0006, 0.00, 0},
-		// 	IterativePosPIDController::Gains{0.0006, 0, 0},
-		// 	AbstractMotor::gearset::green,
-		// 	{2.75_in / 1.6, chassisWidth * 2}
-		// );
-
-
-		// robotProfile = std::make_shared<AsyncMotionProfileController>(AsyncControllerFactory::motionProfile
-		// 	(
-		// 		1.0, 2.0, 10.0,
-		// 		*model
-		// 	));
-
 		pros::delay(200);
-
 		tracker->resetSensors();
 		tracker->resetState();
 	}
+
 
 
 
@@ -141,93 +160,102 @@ void initializeBase()
 	*/
 	#else //TEST_ROBOT
 
-
-	const int8_t left_mPort = 1;
-	const int8_t right_mPort = -2;
-
+	const int8_t e_m_RightFront = -10;
+	const int8_t e_m_RightBack = -2;
+	const int8_t e_m_LeftFront = 9;
+	const int8_t e_m_LeftBack = 1;
 
 	void initializeDevices()
 	{
-		s_leftEncoder = new okapi::ADIEncoder(3, 4);
-		s_rightEncoder = new okapi::ADIEncoder(5, 6);
 		s_middleEncoder = new okapi::ADIEncoder(8, 7);
 
-		s_leftEncoder->reset();
-		s_rightEncoder->reset();
 		s_middleEncoder->reset();
 	}
 
 	void initializeBase()
 	{
 
-		const QLength chassisWidth = 27_cm;
-
-		// Otherwise, you should specify the gearset and scales for your robot
-		model = ChassisControllerFactory::createPtr(
-			left_mPort, right_mPort,
-			*s_leftEncoder, *s_rightEncoder,
-			IterativePosPIDController::Gains{0.001, 0.00, 0},
-			IterativePosPIDController::Gains{0.0006, 0.00, 0},
-			IterativePosPIDController::Gains{0.0006, 0, 0},
-			AbstractMotor::gearset::green,
-			{4_in, chassisWidth * 2}
-		);
+		model = std::make_shared<ThreeEncoderSkidSteerModel>(
+			std::make_shared<MotorGroup>(std::initializer_list<Motor>({e_m_LeftFront, e_m_LeftBack})),
+			std::make_shared<MotorGroup>(std::initializer_list<Motor>({e_m_RightFront, e_m_RightBack})),
+			std::make_shared<IntegratedEncoder>(okapi::Motor(e_m_LeftFront)),
+			std::make_shared<IntegratedEncoder>(okapi::Motor(e_m_RightFront)),
+			std::make_shared<ADIEncoder>(*s_rightEncoder),
+			200,
+			12000);
 
 
-		tracker = new lib7842::OdomTracker
-		(
-			s_leftEncoder, s_rightEncoder, s_middleEncoder,
-			chassisWidth, 18_cm,
-			4_in,
-			360, 360
-		);
+			tracker = new lib7842::OdomTracker
+			(
+				model,
+				10_in, 8_in,
+				4.125_in,
+				360, 360
+			);
 
-		chassis = new lib7842::OdomController(model, tracker);
 
-		pros::delay(500);
-		tracker->resetSensors();
-		tracker->resetState();
-	}
+			chassis = new lib7842::OdomController
+			(
+				tracker,
+				new lib7842::PID(0.00001, 0, 1, 50, 5, 250_ms), //Distance PID - To mm
+				new lib7842::PID(0.00005, 0, 1, 50, 5, 250_ms), //Angle PID - To Degree
+				new lib7842::PID(0.00005, 0.005, 0.9, 3, 1, 100_ms) //Turn PID - To Degree
+			);
+
+
+			pros::delay(200);
+			tracker->resetSensors();
+			tracker->resetState();
+		}
+
+
+
+	// const int8_t left_mPort = 1;
+	// const int8_t right_mPort = -2;
+	//
+	//
+	// void initializeDevices()
+	// {
+	// 	s_leftEncoder = new okapi::ADIEncoder(3, 4);
+	// 	s_rightEncoder = new okapi::ADIEncoder(5, 6);
+	// 	s_middleEncoder = new okapi::ADIEncoder(8, 7);
+	//
+	// 	s_leftEncoder->reset();
+	// 	s_rightEncoder->reset();
+	// 	s_middleEncoder->reset();
+	// }
+	//
+	// void initializeBase()
+	// {
+	//
+	// 	const QLength chassisWidth = 27_cm;
+	//
+	// 	// Otherwise, you should specify the gearset and scales for your robot
+	// 	model = ChassisControllerFactory::createPtr(
+	// 		left_mPort, right_mPort,
+	// 		*s_leftEncoder, *s_rightEncoder,
+	// 		IterativePosPIDController::Gains{0.001, 0.00, 0},
+	// 		IterativePosPIDController::Gains{0.0006, 0.00, 0},
+	// 		IterativePosPIDController::Gains{0.0006, 0, 0},
+	// 		AbstractMotor::gearset::green,
+	// 		{4_in, chassisWidth * 2}
+	// 	);
+	//
+	//
+	// 	tracker = new lib7842::OdomTracker
+	// 	(
+	// 		s_leftEncoder, s_rightEncoder, s_middleEncoder,
+	// 		chassisWidth, 18_cm,
+	// 		4_in,
+	// 		360, 360
+	// 	);
+	//
+	// 	chassis = new lib7842::OdomController(model, tracker);
+	//
+	// 	pros::delay(500);
+	// 	tracker->resetSensors();
+	// 	tracker->resetState();
+	// }
 
 
 	#endif //COMPETITION_ROBOT
-
-
-
-
-
-	void setFlywheelPower(double speed)
-	{
-		m_Flywheel->moveVoltage(speed/127.0*12000);
-		m_Flywheel2->moveVoltage(speed/127.0*12000);
-	}
-	double getFlywheelRPM() { return m_Flywheel->getActualVelocity() * 15; } // 1:15 ratio from motor output to flywhel speed
-
-	void setIntakePower(double speed) { m_Intake->moveVoltage(speed/127.0*12000); }
-	void setIndexerPower(double speed) { m_Indexer->moveVoltage(speed/127.0*12000); }
-	void setIntakeVelocity(double speed) { m_Intake->moveVelocity(speed); }
-	void setIndexerVelocity(double speed) {	m_Indexer->moveVelocity(speed); }
-
-	double getIndexerSensor() {	return s_indexerSensor->get_value_calibrated(); }
-
-
-	void checkBaseStatus()
-	{
-		if(model == nullptr)
-		{
-			std::cout << "USING BASE BEFORE INIT\n";
-			pros::delay(500);
-		}
-	}
-
-	void setBaseArcade(double yPower, double zPower)
-	{
-		checkBaseStatus();
-		model->arcade(yPower, zPower, 0);
-	}
-
-	void setBasePower(double leftPower, double rightPower)
-	{
-		checkBaseStatus();
-		model->tank(leftPower, rightPower, 0);
-	}
