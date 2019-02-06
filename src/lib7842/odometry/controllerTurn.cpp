3bbdddfb5d8c148184c5d9bcdc8b1423c6e6a362
theol0403 <theol0403@gmail.com>
2019-02-06 14:01:36 -0800

    turn to closest angle

--- src/lib7842/odometry/controllerTurn.cpp
+++ src/lib7842/odometry/controllerTurn.cpp
@@ -3,12 +3,16 @@
 namespace lib7842
 {
 
-  void OdomController::turnToAngle(QAngle angle, turnFunc_t turnFunc, settleFunc_t settleFunc)
+  void OdomController::turnToAngle(QAngle angle, bool takeShortest, turnFunc_t turnFunc, settleFunc_t settleFunc)
   {
     angle = rollAngle180(angle);
     turnPid->reset();
     do {
       m_angleErr = rollAngle180(angle - chassis->state.theta);
+      if(takeShortest && m_angleErr.abs() > 90_deg) {
+        m_angleErr += 180_deg;
+        m_angleErr = rollAngle180(m_angleErr);
+      }
       double turnVel = turnPid->calculateErr(m_angleErr.convert(degree));
       turnFunc(this, turnVel);
       pros::delay(10);
@@ -17,18 +21,22 @@ namespace lib7842
     turnFunc(this, 0);
   }
 
-  void OdomController::turnAngle(QAngle angle, turnFunc_t turnFunc, settleFunc_t settleFunc)
+  void OdomController::turnAngle(QAngle angle, bool takeShortest, turnFunc_t turnFunc, settleFunc_t settleFunc)
   {
-    turnToAngle(angle + chassis->state.theta, turnFunc, settleFunc);
+    turnToAngle(angle + chassis->state.theta, takeShortest, turnFunc, settleFunc);
   }
 
 
-  void OdomController::turnToPoint(qPoint point, turnFunc_t turnFunc, settleFunc_t settleFunc)
+  void OdomController::turnToPoint(qPoint point, bool takeShortest, turnFunc_t turnFunc, settleFunc_t settleFunc)
   {
     turnPid->reset();
     do
     {
       m_angleErr = computeAngleToPoint(point);
+      if(takeShortest && m_angleErr.abs() > 90_deg) {
+        m_angleErr += 180_deg;
+        m_angleErr = rollAngle180(m_angleErr);
+      }
       double turnVel = turnPid->calculateErr(m_angleErr.convert(degree));
       turnFunc(this, turnVel);
       pros::delay(10);
