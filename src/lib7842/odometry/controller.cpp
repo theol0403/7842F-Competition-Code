4500863cf3ae1e4e617c12c3782bf049b1b78afa
theol0403 <theol0403@gmail.com>
2019-02-06 12:18:19 -0800

    better?

--- src/lib7842/odometry/controller.cpp
+++ src/lib7842/odometry/controller.cpp
@@ -25,12 +25,20 @@ namespace lib7842
 
 
   turnFunc_t makeArc(double leftRatio, double rightRatio) { return [=](OdomController* that, double turnVel) {
+    turnVel = std::clamp(turnVel, -1.0, 1.0);
+    double leftVel = turnVel * leftRatio;
+    double rightVel = turnVel * rightRatio;
+    double maxMag = std::max(std::abs(leftVel), std::abs(rightVel));
+    if(maxMag > 1) {
+      leftVel = (leftVel / maxMag);
+      rightVel = (rightVel / maxMag);
+    }
     if(leftRatio > rightRatio) {
-      that->chassis->model->left(turnVel * leftRatio);
-      that->chassis->model->right(turnVel * rightRatio);
+      that->chassis->model->left(leftVel);
+      that->chassis->model->right(rightVel);
     } else if(leftRatio < rightRatio) {
-      that->chassis->model->left(-turnVel * leftRatio);
-      that->chassis->model->right(-turnVel * rightRatio);
+      that->chassis->model->left(-leftVel);
+      that->chassis->model->right(-rightVel);
     } else {
       that->chassis->model->rotate(turnVel);
     }
@@ -47,7 +55,7 @@ namespace lib7842
   void rightPivot(OdomController* that, double turnVel) {
     that->chassis->model->right(-turnVel * 2);
   }
-  
+
 
 
   OdomController::OdomController(
