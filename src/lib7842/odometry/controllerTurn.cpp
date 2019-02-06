178dafb7a71b9f491dab43c126a77ac78acda579
theol0403 <theol0403@gmail.com>
2019-02-05 16:03:16 -0800

    pivot turns

--- src/lib7842/odometry/controllerTurn.cpp
+++ src/lib7842/odometry/controllerTurn.cpp
@@ -3,6 +3,18 @@
 namespace lib7842
 {
 
+  void OdomController::turnPoint(double turnVel) {
+    chassis->model->rotate(turnVel);
+  }
+
+  void OdomController::turnPivotLeft(double turnVel) {
+    chassis->model->left(turnVel * 2);
+  }
+
+  void OdomController::turnPivotRight(double turnVel) {
+    chassis->model->right(-turnVel * 2);
+  }
+
   void OdomController::turnToAngle(QAngle angle, settleFunc_t settleFunction)
   {
     angle = rollAngle180(angle);
