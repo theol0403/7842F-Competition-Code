468ae8aa76da0d7277ec55685aa4dfb79d0173a7
theol0403 <theol0403@gmail.com>
2019-02-07 13:20:07 -0800

    move arcs

--- src/lib7842/odometry/controller.cpp
+++ src/lib7842/odometry/controller.cpp
@@ -3,6 +3,10 @@
 namespace lib7842
 {
 
+  /**
+  * Settle Functions
+  */
+ 
   settleFunc_t makeSettle(QAngle threshold) {
     return [=](OdomController* that){ return that->m_angleErr.abs() < threshold || that->emergencyAbort(); };
   }
@@ -22,40 +26,6 @@ namespace lib7842
   bool driveSettle(OdomController* that) {
     return (that->distancePid->isSettled() && that->anglePid->isSettled()) || that->emergencyAbort();
   }
-
-
-  turnFunc_t makeArc(double leftRatio, double rightRatio) { return [=](OdomController* that, double turnVel) {
-    turnVel = std::clamp(turnVel, -1.0, 1.0);
-    double leftVel = turnVel * leftRatio;
-    double rightVel = turnVel * rightRatio;
-    double maxMag = std::max(std::abs(leftVel), std::abs(rightVel));
-    if(maxMag > 1) {
-      leftVel = (leftVel / maxMag);
-      rightVel = (rightVel / maxMag);
-    }
-    if(leftRatio > rightRatio) {
-      that->chassis->model->left(leftVel);
-      that->chassis->model->right(rightVel);
-    } else if(leftRatio < rightRatio) {
-      that->chassis->model->left(-leftVel);
-      that->chassis->model->right(-rightVel);
-    } else {
-      that->chassis->model->rotate(turnVel);
-    }
-  };}
-
-  void pointTurn(OdomController* that, double turnVel) {
-    that->chassis->model->rotate(turnVel);
-  }
-
-  void leftPivot(OdomController* that, double turnVel) {
-    that->chassis->model->left(turnVel * 2);
-  }
-
-  void rightPivot(OdomController* that, double turnVel) {
-    that->chassis->model->right(-turnVel * 2);
-  }
-
 
 
   OdomController::OdomController(
--- src/lib7842/odometry/controllerTurn.cpp
+++ src/lib7842/odometry/controllerTurn.cpp
@@ -3,6 +3,40 @@
 namespace lib7842
 {
 
+  turnFunc_t makeArc(double leftRatio, double rightRatio) { return [=](OdomController* that, double turnVel) {
+    turnVel = std::clamp(turnVel, -1.0, 1.0);
+    double leftVel = turnVel * leftRatio;
+    double rightVel = turnVel * rightRatio;
+    double maxMag = std::max(std::abs(leftVel), std::abs(rightVel));
+    if(maxMag > 1) {
+      leftVel = (leftVel / maxMag);
+      rightVel = (rightVel / maxMag);
+    }
+    if(leftRatio > rightRatio) {
+      that->chassis->model->left(leftVel);
+      that->chassis->model->right(rightVel);
+    } else if(leftRatio < rightRatio) {
+      that->chassis->model->left(-leftVel);
+      that->chassis->model->right(-rightVel);
+    } else {
+      that->chassis->model->rotate(turnVel);
+    }
+  };}
+
+  void pointTurn(OdomController* that, double turnVel) {
+    that->chassis->model->rotate(turnVel);
+  }
+
+  void leftPivot(OdomController* that, double turnVel) {
+    that->chassis->model->left(turnVel * 2);
+  }
+
+  void rightPivot(OdomController* that, double turnVel) {
+    that->chassis->model->right(-turnVel * 2);
+  }
+
+
+
   void OdomController::turnToAngle(QAngle angle, turnFunc_t turnFunc, settleFunc_t settleFunc)
   {
     resetEmergencyAbort();
@@ -24,6 +58,7 @@ namespace lib7842
   }
 
 
+
   void OdomController::turnToPoint(qPoint point, turnFunc_t turnFunc, settleFunc_t settleFunc)
   {
     resetEmergencyAbort();
@@ -38,6 +73,8 @@ namespace lib7842
     while(!settleFunc(this));
     turnFunc(this, 0);
   }
+
+
 
 
 }
