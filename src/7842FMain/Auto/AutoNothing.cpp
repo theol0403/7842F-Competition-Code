fcd4a59cdfc501a5d6c298d0744513710efd42aa
theol0403 <theol0403@gmail.com>
2019-02-09 16:28:03 -0800

    before working on triggers

--- src/7842FMain/Auto/AutoNothing.cpp
+++ src/7842FMain/Auto/AutoNothing.cpp
@@ -9,9 +9,8 @@ void AutoTest(lib7842::autonSides side)
 {
   // chassis->driveDistance(1_ft);
   // chassis->driveToPoint({2_ft, 1_ft}, 3, makeSettle(1_in));
-  // chassis->driveToPoint({0_in, 0_ft}, 3, makeSettle(1_in));
-  chassis->drivePathSimple({{0_in, 2_ft}, {2_ft, 2_ft}, {0_ft, 0_ft}}, 4, makeSettle(2_in));
-  volatile int test = 0;
+  //chassis->driveToPoint({0_in, 1_ft}, 3, driveSettle);
+  chassis->drivePath({{0_in, 2_ft}, {2_ft, 2_ft}, {0_ft, 0_ft}}, 4, makeSettle(4_in));
 
 
 }
