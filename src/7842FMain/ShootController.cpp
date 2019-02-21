4bed2a4c2c2f443e88a3a348d5f5244a6aa20977
theol0403 <theol0403@gmail.com>
2019-02-21 10:58:23 -0800

    work on angling

--- src/7842FMain/ShootController.cpp
+++ src/7842FMain/ShootController.cpp
@@ -71,151 +71,114 @@ QLength ShootController::getDistanceToFlag() {
 }
 
 double ShootController::getTopFlagAngle() {
-  switch((int)getDistanceToFlag().convert(inch))
+  switch((int) getDistanceToFlag().convert(foot))
   {
-    case 0 ... 12 :
-    {
-      return 0;
-      break;
-    }
-    case 13 ... 24 :
-    {
-      return 30;
-      break;
-    }
-    case 25 ... 36 :
-    {
-      return 60;
-      break;
-    }
-    case 37 ... 48 :
-    {
-      return 90;
-      break;
-    }
-    default :
-    {
-      return 100;
-      break;
-    }
+    case 0 : return 0; break;
+    case 1 : return 30; break;
+    case 2 : return 60; break;
+    case 3 : return 90; break;
+    case 4 : return 120; break;
+    default : return 150; break;
   }
   return 0;
 }
 
 double ShootController::getMiddleFlagAngle() {
-  switch((int)getDistanceToFlag().convert(inch))
+  switch((int) getDistanceToFlag().convert(foot))
   {
-    case 0 ... 12 :
-    {
-      return 0;
-      break;
-    }
-    case 13 ... 24 :
-    {
-      return 60;
-      break;
-    }
-    case 25 ... 36 :
-    {
-      return 90;
-      break;
-    }
-    case 37 ... 48 :
-    {
-      return 120;
-      break;
-    }
-    default :
-    {
-      return 100;
-      break;
-    }
+    case 0 : return 30; break;
+    case 1 : return 60; break;
+    case 2 : return 90; break;
+    case 3 : return 120; break;
+    case 4 : return 150; break;
+    default : return 180; break;
   }
   return 0;
 }
 
 void ShootController::run()
 {
-  const double angleThresh = 30;
+  const double angleThresh = 15;
 
   while(true)
   {
 
     switch(getCurrentJob())
     {
+
       case shootStates::standby:
-      {
-        if(getAngle() > 0 + angleThresh) {
-          addJob(shootStates::cycle);
-        } else {
-          flywheel->enable();
-        }
-        break;
+
+      if(getAngle() >= angleThresh) {
+        addJob(shootStates::cycle);
+      } else {
+        flywheel->enable();
       }
+      break;
 
       case shootStates::cycle:
-      {
+
+      flywheel->disable();
+      flywheel->flywheel->moveVelocity(-50);
+      //If angle is within error of 0
+      if(getAngle() <= angleThresh) {
+        flywheel->enable();
+        completeJob();
+      }
+      break;
+
+
+      case shootStates::angleTop:
+
+      if(getAngle() > getTopFlagAngle() + angleThresh) {
+        addJob(shootStates::cycle);
+      } else {
         flywheel->disable();
         flywheel->flywheel->moveVelocity(-50);
-        if(getAngle() < angleThresh) {
+        if(getAngle() >= getTopFlagAngle()) {
           flywheel->enable();
           completeJob();
         }
-        break;
       }
+      break;
 
-      case shootStates::angleTop:
-      {
-        if(getAngle() > getTopFlagAngle() + angleThresh) {
-          addJob(shootStates::cycle);
-        } else {
-          flywheel->disable();
-          flywheel->flywheel->moveVelocity(-50);
-          if(getAngle() > getTopFlagAngle()) {
-            flywheel->enable();
-            completeJob();
-          }
-        }
-        break;
-      }
 
       case shootStates::angleMiddle:
-      {
-        if(getAngle() > getMiddleFlagAngle() + angleThresh) {
-          addJob(shootStates::cycle);
-        } else {
-          flywheel->disable();
-          flywheel->flywheel->moveVelocity(-50);
-          if(getAngle() > getMiddleFlagAngle()) {
-            flywheel->enable();
-            completeJob();
-          }
+
+      if(getAngle() > getMiddleFlagAngle() + angleThresh) {
+        addJob(shootStates::cycle);
+      } else {
+        flywheel->disable();
+        flywheel->flywheel->moveVelocity(-50);
+        if(getAngle() >= getMiddleFlagAngle()) {
+          flywheel->enable();
+          completeJob();
         }
-        break;
       }
+      break;
+
 
       case shootStates::waitForFlywheel:
-      {
-        flywheel->enable();
-        if(flywheel->pid->getError() < 100) completeJob();
-        break;
-      }
+
+      flywheel->enable();
+      if(flywheel->pid->getError() < 100) completeJob();
+      break;
+
 
       case shootStates::shootIndexer:
-      {
-        flywheel->enable();
-        intake->setState(IntakeController::shootIndexer);
-        if(intake->getState() != IntakeController::shootIndexer) completeJob();
-        break;
-      }
+
+      flywheel->enable();
+      intake->setState(IntakeController::shootIndexer);
+      if(intake->getState() != IntakeController::shootIndexer) completeJob();
+      break;
+
 
       case shootStates::shootBoth:
-      {
-        flywheel->enable();
-        intake->setState(IntakeController::shootBoth);
-        if(intake->getState() != IntakeController::shootBoth) completeJob();
-        break;
-      }
+
+      flywheel->enable();
+      intake->setState(IntakeController::shootBoth);
+      if(intake->getState() != IntakeController::shootBoth) completeJob();
+      break;
+
 
     }
 
