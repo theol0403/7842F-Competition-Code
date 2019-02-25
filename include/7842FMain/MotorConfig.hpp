bc611ce3cb587b7a257b9f5901fba3012893023a
theol0403 <theol0403@gmail.com>
2019-02-25 13:10:46 -0800

    small changes

--- include/7842FMain/MotorConfig.hpp
+++ include/7842FMain/MotorConfig.hpp
@@ -31,4 +31,4 @@ void initializeBase();
 void checkBaseStatus();
 
 
-#define TEST_ROBOT
+//#define TEST_ROBOT
--- src/7842FMain/DriverControl.cpp
+++ src/7842FMain/DriverControl.cpp
@@ -132,7 +132,7 @@ void driverControl()
 			robot.flywheel->setRpm(0);
 		}
 		robot.shooter->doJob(ShootController::off);
-		robot.arm->setState(ArmController::unfold);
+		robot.arm->setState(ArmController::unfold); //for testing
 	}
 
 }
