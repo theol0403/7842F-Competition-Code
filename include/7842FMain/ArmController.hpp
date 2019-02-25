1f467f58c62c6f08026879d0866195deff8b2e25
theol0403 <theol0403@gmail.com>
2019-02-24 17:06:32 -0800

    start on arm

---
+++ include/7842FMain/ArmController.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include "main.h"
+
+class IntakeController
+{
+
+public:
+
+  enum intakeStates
+  {
+    off,
+    intakeBall, //runs intake and indexer until ball then indexer stops
+    outIntake
+  };
+
+  Motor* intake = nullptr;
+  Motor* indexer = nullptr;
+  pros::ADILineSensor* lineSensor = nullptr;
+  okapi::EmaFilter sensorFilter;
+  pros::Task intakeTask;
+
+  intakeStates intakeState = off;
+  bool hasBall = false;
+  bool disabled = false;
+  bool indexerSlave = true;
+
+  IntakeController(Motor*, Motor*, pros::ADILineSensor*, double);
+
+  void setState(intakeStates);
+  intakeStates getState();
+  void disable();
+  void enable();
+
+  void run();
+
+  static void task(void*);
+
+};
---
+++ src/7842FMain/ArmController.cpp
@@ -0,0 +1,79 @@
+#include "IntakeController.hpp"
+
+IntakeController::IntakeController(Motor* iintake, Motor* iindexer, pros::ADILineSensor* ilineSensor, double isensorEma) :
+intake(iintake), indexer(iindexer), lineSensor(ilineSensor), sensorFilter(isensorEma),
+intakeTask(task, this)
+{
+  intake->setBrakeMode(AbstractMotor::brakeMode::brake);
+  //indexer->setBrakeMode(AbstractMotor::brakeMode::hold);
+  lineSensor->calibrate();
+}
+
+void IntakeController::setState(intakeStates state) {
+  intakeState = state;
+}
+
+IntakeController::intakeStates IntakeController::getState() {
+  return intakeState;
+}
+
+void IntakeController::disable() {
+  if(!disabled) {
+    intake->moveVelocity(0);
+    indexer->moveVelocity(0);
+  }
+  disabled = true;
+}
+
+void IntakeController::enable() {
+  disabled = false;
+}
+
+
+void IntakeController::run()
+{
+
+  while(true)
+  {
+    double filteredSensor = sensorFilter.filter(lineSensor->get_value_calibrated());
+    //std::cout << "Sensor: " << filteredSensor << std::endl;
+    hasBall = filteredSensor < 50;
+
+    if(!disabled)
+    {
+      switch(intakeState) {
+
+        case intakeStates::off:
+        intake->moveVelocity(0);
+        indexerSlave = true;
+        break;
+
+        case intakeStates::intakeBall:
+        intake->moveVelocity(200);
+        if(hasBall) {
+          indexerSlave = true;
+        } else {
+          indexerSlave = false;
+          indexer->moveVelocity(80);
+        }
+        break;
+
+        case intakeStates::outIntake:
+        intake->moveVelocity(-200);
+        indexerSlave = true;
+        break;
+
+      }
+
+    }
+
+    pros::delay(10);
+  }
+}
+
+
+void IntakeController::task(void* input)
+{
+  IntakeController* that = static_cast<IntakeController*>(input);
+  that->run();
+}
