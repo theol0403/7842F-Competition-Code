6ca3ce53400ecf2d7aeb711eb7a56564254de52e
theol0403 <theol0403@gmail.com>
2019-03-04 13:50:08 -0800

    custom avg filter

---
+++ include/lib7842/filter/avgFilter.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <vector>
+#include <cstddef>
+
+namespace lib7842
+{
+  class avgFilter
+  {
+
+  private:
+
+    const int size;
+    std::vector<double> data {0};
+    std::size_t index = 0;
+    double output = 0;
+
+  public:
+
+    avgFilter(int);
+
+    double filter(double);
+    double getOutput();
+
+  };
+}
---
+++ src/lib7842/filter/avgFilter.cpp
@@ -0,0 +1,27 @@
+#include "avgFilter.hpp"
+
+namespace lib7842
+{
+
+  avgFilter::avgFilter(int isize) : size(isize) { data.resize(size); }
+
+  double avgFilter::filter(double input) {
+    data.at(index++) = input;
+    if (index >= size) {
+      index = 0;
+    }
+
+    output = 0.0;
+    for(size_t i = 0; i < size; i++)
+    output += data.at(i);
+    output /= (double) size;
+
+    return output;
+  }
+
+
+  double avgFilter::getOutput() {
+    return output;
+  }
+
+}
