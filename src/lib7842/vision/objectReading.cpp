4d5c65fbe138471f61e2de857d4aada661ddd790
theol0403 <theol0403@gmail.com>
2019-03-16 17:36:37 -0700

    assert sig

--- src/lib7842/vision/objectReading.cpp
+++ src/lib7842/vision/objectReading.cpp
@@ -3,9 +3,7 @@
 namespace lib7842
 {
 
-  ObjectReading::ObjectReading(pros::Vision* ivision) : vision(ivision), maxCount(20) {
-    temp.resize(maxCount);
-  }
+  ObjectReading::ObjectReading(pros::Vision* ivision) : vision(ivision), maxCount(20) { temp.resize(maxCount); }
 
   ObjectReading::ObjectReading(pros::Vision& ivision) : ObjectReading(&ivision) {}
 
@@ -24,7 +22,10 @@ namespace lib7842
     int count = vision->read_by_sig(0, sig, maxCount, temp.data());
     if(count > maxCount) count = 0; //If there are no objects pros returns a huge number
 
-    for (int i = 0; i < count; i++) addObj(temp.at(i));
+    for (int i = 0; i < count; i++) {
+      assert(temp.at(i).signature == sig);
+      addObj(temp.at(i));
+    }
   }
 
   void ObjectReading::getSig(std::initializer_list<int> sigs)
@@ -33,41 +34,6 @@ namespace lib7842
   }
 
 
-
-  //
-  // void ObjectReading::getSigObjects(lib7842::ObjectContainer &destContainer, std::initializer_list<int> wantedSignatures)
-  // {
-  //   pros::vision_object* temp = new pros::vision_object[destContainer.arrayLength];
-  //
-  //   for(const int wantedSig : wantedSignatures)
-  //   {
-  //     int readCount = m_thisVision->read_by_sig(0, wantedSig, destContainer.arrayLength, temp);
-  //     if(readCount > destContainer.arrayLength) readCount = 0; //If there are no objects pros returns a huge number
-  //
-  //     int goodCount = 0;
-  //
-  //     for (int i = 0; i < readCount; i++)
-  //     {
-  //       if(temp[i].signature != VISION_OBJECT_ERR_SIG)
-  //       {
-  //         if(temp[i].signature != wantedSig) { std::cout << "REEEE: " << temp[i].signature << "\n"; }
-  //         destContainer.objectArray.at(i + destContainer.currentCount).objSig = temp[i].signature;
-  //         destContainer.objectArray.at(i + destContainer.currentCount).objX = temp[i].left_coord;
-  //         destContainer.objectArray.at(i + destContainer.currentCount).objY = temp[i].top_coord;
-  //         destContainer.objectArray.at(i + destContainer.currentCount).objWidth = temp[i].width;
-  //         destContainer.objectArray.at(i + destContainer.currentCount).objHeight = temp[i].height;
-  //         destContainer.objectArray.at(i + destContainer.currentCount).objArea = temp[i].height * temp[i].width;
-  //         destContainer.objectArray.at(i + destContainer.currentCount).objCenterX = temp[i].x_middle_coord;
-  //         destContainer.objectArray.at(i + destContainer.currentCount).objCenterY = temp[i].y_middle_coord;
-  //         destContainer.objectArray.at(i + destContainer.currentCount).discardObject = false;
-  //         goodCount++;
-  //       }
-  //     }
-  //     destContainer.currentCount += goodCount;
-  //   }
-  //   delete[] temp;
-  // }
-  //
   // void ObjectReading::getCodeObjects(lib7842::ObjectContainer &destContainer, std::vector<codeSig_t> wantedCodes)
   // {
   //   pros::vision_object* temp = new pros::vision_object[destContainer.arrayLength];
