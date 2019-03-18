a1bb3982f0c9dbe6411f8b33a18dd984497e557a
theol0403 <theol0403@gmail.com>
2019-03-18 13:53:51 -0700

    format

--- src/lib7842/other/sdLogger.cpp
+++ src/lib7842/other/sdLogger.cpp
@@ -35,8 +35,8 @@ namespace lib7842
         }
         do {
           path = "/usd/" + name + std::to_string(fileNum) + ".csv";
-          fileNum++;
           std::cout << "Trying: " << path << std::endl;
+          fileNum++;
         }
         while(fileExists(path));
         FILE* file = fopen(name.c_str(), "w");
@@ -48,7 +48,13 @@ namespace lib7842
       case time: {
         pros::date_s_t date = pros::get_date();
         pros::time_s_t time = pros::get_time();
-        path = "/usd/" + name + std::to_string(date.year) + "-" + std::to_string(date.month) + "-" + std::to_string(date.day) + "_" + std::to_string(time.hour) + "-" + std::to_string(time.min) + ".csv";
+        //%Y%m%dT%H%M%S
+        path = "/usd/" + name + std::to_string(date.year) + std::to_string(date.month) + std::to_string(date.day) + "T" + std::to_string(time.hour) + std::to_string(time.min) + std::to_string(time.sec) + ".csv";
+        break;
+      }
+
+      case cout: {
+        path = "/ser/sout";
         break;
       }
 
@@ -75,10 +81,5 @@ namespace lib7842
       //std::cout << "REOPEN" << std::endl;
     }
   }
-
-
-
-
-
 
 }
