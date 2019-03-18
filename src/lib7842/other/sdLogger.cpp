da5dd5f98eef7e097bb154c52245a46c9d25a357
theol0403 <theol0403@gmail.com>
2019-03-18 12:19:28 -0700

    yikes

--- src/lib7842/other/sdLogger.cpp
+++ src/lib7842/other/sdLogger.cpp
@@ -24,18 +24,38 @@ namespace lib7842
 
       case count: {
         int fileNum = 0;
+        if(fileExists(name)) {
+          FILE* file = fopen(name.c_str(), "r");
+          fileNum = getw(file);
+          fclose(file);
+        } else {
+          std::cout << "Opening" << std::endl;
+          pros::delay(200);
+          FILE* file = fopen(name.c_str(), "w");
+          std::cout << "Putting" << std::endl;
+          pros::delay(200);
+          putw(0, file);
+          std::cout << "Closing" << std::endl;
+          pros::delay(200);
+          fclose(file);
+        }
+        std::cout << "Num: " << fileNum << std::endl;
+        pros::delay(1000);
         do {
           path = "/usd/" + name + std::to_string(fileNum) + ".csv";
           fileNum++;
           std::cout << "Trying: " << path << std::endl;
         }
         while(fileExists(path));
+        FILE* file = fopen(name.c_str(), "w");
+        putw(fileNum, file);
+        fclose(file);
         break;
       }
 
       case time: {
-        date_s_t date = get_date();
-        time_s_t time = get_time();
+        pros::date_s_t date = pros::get_date();
+        pros::time_s_t time = pros::get_time();
         path = "/usd/" + name + std::to_string(date.year) + "-" + std::to_string(date.month) + "-" + std::to_string(date.day) + "_" + std::to_string(time.hour) + "-" + std::to_string(time.min) + ".csv";
         break;
       }
