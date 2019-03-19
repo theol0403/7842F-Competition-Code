aafd3b3bea01be9f5dc5a911d2f3b6d79402c1a2
theol0403 <theol0403@gmail.com>
2019-03-19 11:05:29 -0700

    work

--- src/7842FMain/VisionController.cpp
+++ src/7842FMain/VisionController.cpp
@@ -15,16 +15,21 @@ void VisionController::run()
   sensor->set_exposure(83);
   pros::vision_signature_s_t SIG_1 = {1, {1, 0, 0}, 7.700, -2657, -2461, -2559, 4939, 5765, 5352, 0, 0}; sensor->set_signature(1, &SIG_1);
   pros::vision_signature_s_t SIG_2 = {2, {1, 0, 0}, 5.800, 6425, 7147, 6786, -329, -103, -216, 0, 0}; sensor->set_signature(2, &SIG_2);
+
   ObjDrawer drawer(lv_scr_act());
   drawer.withStyle(LV_COLOR_HEX(0xFF7F00), LV_COLOR_WHITE);
-  drawer.withLayer(reader).withStyle(VISION_OBJECT_ERR_SIG, LV_COLOR_YELLOW, LV_COLOR_WHITE).withStyle(1, LV_COLOR_BLUE, LV_COLOR_WHITE).withStyle(2, LV_COLOR_RED, LV_COLOR_WHITE);
+
+  drawer.withLayer(reader)
+  .withStyle(VISION_OBJECT_ERR_SIG, LV_COLOR_YELLOW, LV_COLOR_WHITE)
+  .withStyle(1, LV_COLOR_BLUE, LV_COLOR_WHITE)
+  .withStyle(2, LV_COLOR_RED, LV_COLOR_WHITE);
 
   while(true)
   {
     reader.reset();
     reader.getAll();
-    reader.print();
     reader.removeWith(objAttr::area, 0, 300);
+    reader.print();
     drawer.draw();
     pros::delay(100);
   }
