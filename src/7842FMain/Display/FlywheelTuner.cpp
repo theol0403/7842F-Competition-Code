394e1efc8b83e0371bae90e0beebe27d840892a5
theol0403 <theol0403@gmail.com>
2019-03-20 14:56:47 -0700

    label work

--- src/7842FMain/Display/FlywheelTuner.cpp
+++ src/7842FMain/Display/FlywheelTuner.cpp
@@ -28,20 +28,24 @@ FlywheelTuner &FlywheelTuner::withButton(std::string name, double* variable, but
 
 void FlywheelTuner::build() {
 
+  std::vector<std::string> names;
+  std::vector<const char*>* btnLabels = new std::vector<const char*>;
+
   for(auto &button : buttons) {
-    names.push_back("+");
+    names.push_back(button.first);
+    btnLabels->push_back("+");
   }
-  names.push_back("\n");
+  btnLabels->push_back("\n");
   for(auto &button : buttons) {
-    names.push_back("-");
+    btnLabels->push_back("-");
   }
 
-  names.push_back("");
+  btnLabels->push_back("");
   lv_obj_t* btnm = lv_btnm_create(container, NULL);
-  lv_btnm_set_map(btnm, names.data());
+  lv_btnm_set_map(btnm, btnLabels->data());
 
   lv_obj_set_size(btnm, lv_obj_get_width(container), lv_obj_get_height(container) / 3);
-  lv_obj_align(container, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, 0);
+  lv_obj_align(btnm, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, 0);
   // lv_btnm_set_action(btnm, matrixAction);
   // lv_obj_set_free_ptr(btnm, this);
 
@@ -52,6 +56,9 @@ void FlywheelTuner::build() {
   style_bg->body.border.color = LV_COLOR_WHITE;
   style_bg->body.border.width = 3;
   style_bg->body.radius = 0;
+  style_bg->body.padding.hor = 5;
+  style_bg->body.padding.ver = 5;
+  style_bg->body.padding.inner = 5;
   lv_btnm_set_style(btnm, LV_BTNM_STYLE_BG, style_bg);
 
   lv_style_t* style_rel = new lv_style_t;
@@ -59,9 +66,9 @@ void FlywheelTuner::build() {
   style_rel->body.main_color = mainColor;
   style_rel->body.grad_color = mainColor;
   style_rel->body.border.color = LV_COLOR_WHITE;
-  style_rel->body.border.width = 3;
+  style_rel->body.border.width = 2;
   style_rel->body.border.opa = LV_OPA_100;
-  style_rel->body.radius = 10;
+  style_rel->body.radius = 2;
   style_rel->text.color = LV_COLOR_WHITE;
   lv_btnm_set_style(btnm, LV_BTNM_STYLE_BTN_REL, style_rel);
 
@@ -79,5 +86,13 @@ void FlywheelTuner::build() {
   style_ina->body.border.width = 0;
   style_ina->text.color = LV_COLOR_WHITE;
   lv_btnm_set_style(btnm, LV_BTNM_STYLE_BTN_INA, style_ina);
+
+  double offset = 0;
+  for(std::string &name : names) {
+    lv_obj_t* label = lv_label_create(container, NULL);
+    lv_label_set_text(label, name.c_str());
+    offset += (double)lv_obj_get_width(container)/(names.size()+1);
+    lv_obj_align(label, NULL, LV_ALIGN_IN_BOTTOM_LEFT, offset - lv_obj_get_width(label)/2, -lv_obj_get_height(container)/6 + lv_obj_get_height(label)/2);
+  }
 
 }
