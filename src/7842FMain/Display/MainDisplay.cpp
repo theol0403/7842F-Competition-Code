0229612af8f1aed104680f273774ddb1cadb4a62
theol0403 <theol0403@gmail.com>
2019-03-22 14:25:50 -0700

    splash tweaks

--- src/7842FMain/Display/MainDisplay.cpp
+++ src/7842FMain/Display/MainDisplay.cpp
@@ -27,8 +27,6 @@ tabview(lv_tabview_create(parent, NULL)), mainColor(imainColor)
   style_btn_bg->body.padding.hor = 0;
   style_btn_bg->body.padding.inner = 1;
   style_btn_bg->text.color = LV_COLOR_WHITE;
-  style_btn_bg->body.border.opa = LV_OPA_100;
-  style_btn_bg->body.border.color = LV_COLOR_WHITE;
   lv_tabview_set_style(tabview, LV_TABVIEW_STYLE_BTN_BG, style_btn_bg);
 
   lv_style_t* style_rel = new lv_style_t;
@@ -45,10 +43,9 @@ tabview(lv_tabview_create(parent, NULL)), mainColor(imainColor)
   lv_style_copy(style_tgl_rel, &lv_style_btn_tgl_rel);
   style_tgl_rel->body.main_color = mainColor;
   style_tgl_rel->body.grad_color = mainColor;
-  style_tgl_rel->body.border.color = LV_COLOR_WHITE;
-  style_tgl_rel->body.border.width = 3;
-  style_tgl_rel->body.radius = 0;
   style_tgl_rel->text.color = LV_COLOR_WHITE;
+  style_tgl_rel->body.radius = 0;
+  style_tgl_rel->body.border.width = 3;
   style_tgl_rel->body.border.opa = LV_OPA_100;
   style_tgl_rel->body.border.color = LV_COLOR_WHITE;
   lv_tabview_set_style(tabview, LV_TABVIEW_STYLE_BTN_TGL_REL, style_tgl_rel);
@@ -112,7 +109,7 @@ void MainDisplay::splashScreen(const lv_img_t* imgPtr, int time) {
     lv_style_copy(&style, &lv_style_pretty_color);
     style.body.main_color = display->mainColor;
     style.body.grad_color = display->mainColor;
-    style.body.border.width = 5;
+    style.body.border.width = 3;
     style.body.border.color = LV_COLOR_WHITE;
     style.body.border.opa = LV_OPA_100;
     style.body.radius = 0;
@@ -125,19 +122,33 @@ void MainDisplay::splashScreen(const lv_img_t* imgPtr, int time) {
     lv_style_copy(&imgStyle, &lv_style_plain);
     lv_img_set_style(img, &imgStyle);
 
-    lv_anim_t a;
-    a.var = img; a.start = 10; a.end = 255;
-    a.fp = [](void* iimg, int32_t val)
+    lv_anim_t bAnim;
+    bAnim.var = overlay; bAnim.start = 0; bAnim.end = 255;
+    bAnim.fp = [](void* ioverlay, int32_t val)
+    {
+      lv_obj_t* overlay = static_cast<lv_obj_t*>(ioverlay);
+      lv_style_t* parentStyle = lv_obj_get_style(overlay);
+      parentStyle->body.border.opa = val;
+      lv_obj_set_style(overlay, parentStyle);
+    };
+    bAnim.path = lv_anim_path_linear;
+    bAnim.time = time/2;
+    bAnim.end_cb = NULL; bAnim.act_time = 0; bAnim.playback = 1; bAnim.playback_pause = 0; bAnim.repeat = 0; bAnim.repeat_pause = 0;
+    lv_anim_create(&bAnim);
+
+    lv_anim_t iAnim;
+    iAnim.var = img; iAnim.start = 0; iAnim.end = 255;
+    iAnim.fp = [](void* iimg, int32_t val)
     {
       lv_obj_t* img = static_cast<lv_obj_t*>(iimg);
       lv_style_t* imgStyle = lv_img_get_style(img);
       imgStyle->image.opa = val;
       lv_img_set_style(img, imgStyle);
     };
-    a.path = lv_anim_path_linear;
-    a.time = time/3.0;
-    a.end_cb = NULL; a.act_time = 0; a.playback = 1; a.playback_pause = time/3.0; a.repeat = 0; a.repeat_pause = 0;
-    lv_anim_create(&a);
+    iAnim.path = lv_anim_path_linear;
+    iAnim.time = time/3.0;
+    iAnim.end_cb = NULL; iAnim.act_time = 0; iAnim.playback = 1; iAnim.playback_pause = time/3.0; iAnim.repeat = 0; iAnim.repeat_pause = 0;
+    lv_anim_create(&iAnim);
 
     pros::delay(time);
 
