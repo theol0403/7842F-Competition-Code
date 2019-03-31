e635f4a82ece8c401cf6e098e79e79a3333df547
theol0403 <theol0403@gmail.com>
2019-03-30 22:03:21 -0700

    work on angler tuner

--- src/7842FMain/Display/AngleTuner.cpp
+++ src/7842FMain/Display/AngleTuner.cpp
@@ -12,14 +12,18 @@ container(parent), angler(iangler), task(taskFnc, this)
   int angleContainerHeight = lv_obj_get_height(container);
 
   int actionContainerWidth = lv_obj_get_width(container) - angleContainerWidth;
-  int actionContainerHeight = lv_obj_get_height(container)/4.0;
+  int actionContainerHeight = lv_obj_get_height(container)/5.0;
+
+  int dialContainerWidth = (lv_obj_get_width(container) - angleContainerWidth) / 3;
+  int dialContainerHeight = lv_obj_get_height(container) - actionContainerHeight;
 
   /**
   * ButtonMatrix Styles
   */
   lv_style_t* btnm_bg = new lv_style_t;
   lv_style_copy(btnm_bg, &lv_style_plain);
-  btnm_bg->body.opa = LV_OPA_TRANSP;
+  btnm_bg->body.main_color = mainColor;
+  btnm_bg->body.grad_color = mainColor;
   btnm_bg->body.border.color = LV_COLOR_WHITE;
   btnm_bg->body.border.width = 0;
   btnm_bg->body.radius = 0;
@@ -29,7 +33,8 @@ container(parent), angler(iangler), task(taskFnc, this)
 
   lv_style_t* btnm_rel = new lv_style_t;
   lv_style_copy(btnm_rel, &lv_style_btn_tgl_rel);
-  btnm_rel->body.opa = LV_OPA_TRANSP;
+  btnm_rel->body.main_color = mainColor;
+  btnm_rel->body.grad_color = mainColor;
   btnm_rel->body.border.color = LV_COLOR_WHITE;
   btnm_rel->body.border.width = 2;
   btnm_rel->body.border.opa = LV_OPA_100;
@@ -40,11 +45,12 @@ container(parent), angler(iangler), task(taskFnc, this)
   lv_style_copy(btnm_pr, btnm_rel);
   btnm_pr->body.main_color = LV_COLOR_WHITE;
   btnm_pr->body.grad_color = LV_COLOR_WHITE;
-  btnm_pr->text.opa = LV_OPA_TRANSP;
+  btnm_pr->text.color = mainColor;
 
   lv_style_t* btnm_ina = new lv_style_t;
   lv_style_copy(btnm_ina, &lv_style_btn_ina);
-  btnm_ina->body.opa = LV_OPA_TRANSP;
+  btnm_ina->body.main_color = mainColor;
+  btnm_ina->body.grad_color = mainColor;
   btnm_ina->body.border.width = 0;
   btnm_ina->text.color = LV_COLOR_WHITE;
 
@@ -62,70 +68,79 @@ container(parent), angler(iangler), task(taskFnc, this)
   /**
   * Angle Buttons
   */
-  lv_obj_t* angleContainer = lv_obj_create(container, NULL);
-  lv_obj_set_size(angleContainer, angleContainerWidth, angleContainerHeight);
-  lv_obj_align(angleContainer, NULL, LV_ALIGN_IN_LEFT_MID, 0, 0);
-  lv_obj_set_style(angleContainer, masterStyle);
+  {
+    lv_obj_t* angleContainer = lv_obj_create(container, NULL);
+    lv_obj_set_size(angleContainer, angleContainerWidth, angleContainerHeight);
+    lv_obj_align(angleContainer, NULL, LV_ALIGN_IN_LEFT_MID, 0, 0);
+    lv_obj_set_style(angleContainer, masterStyle);
 
-  std::vector<const char*>* btnLabels = new std::vector<const char*>;
-  *btnLabels = {"Dist+", "\n", "Dist-", "\n", "Top+", "\n", "Top-", "\n", "Mid+", "\n", "Mid-", ""};
+    std::vector<const char*>* btnLabels = new std::vector<const char*>;
+    *btnLabels = {"Dist+", "\n", "Dist-", "\n", "Top+", "\n", "Top-", "\n", "Mid+", "\n", "Mid-", ""};
 
-  lv_obj_t* angleBtnm = lv_btnm_create(angleContainer, NULL);
-  lv_btnm_set_map(angleBtnm, btnLabels->data());
+    lv_obj_t* btnm = lv_btnm_create(angleContainer, NULL);
+    lv_btnm_set_map(btnm, btnLabels->data());
 
-  lv_btnm_set_style(angleBtnm, LV_BTNM_STYLE_BG, btnm_bg);
-  lv_btnm_set_style(angleBtnm, LV_BTNM_STYLE_BTN_REL, btnm_rel);
-  lv_btnm_set_style(angleBtnm, LV_BTNM_STYLE_BTN_PR, btnm_pr);
-  lv_btnm_set_style(angleBtnm, LV_BTNM_STYLE_BTN_INA, btnm_ina);
+    lv_btnm_set_style(btnm, LV_BTNM_STYLE_BG, btnm_bg);
+    lv_btnm_set_style(btnm, LV_BTNM_STYLE_BTN_REL, btnm_rel);
+    lv_btnm_set_style(btnm, LV_BTNM_STYLE_BTN_PR, btnm_pr);
+    lv_btnm_set_style(btnm, LV_BTNM_STYLE_BTN_INA, btnm_ina);
 
-  lv_obj_set_size(angleBtnm, lv_obj_get_width(angleContainer)/3*2, lv_obj_get_height(angleContainer));
-  lv_btnm_set_action(angleBtnm, angleBtnAction);
-  lv_obj_set_free_ptr(angleBtnm, this);
+    lv_obj_set_size(btnm, lv_obj_get_width(angleContainer)/3*2, lv_obj_get_height(angleContainer));
+    lv_btnm_set_action(btnm, angleBtnAction);
+    lv_obj_set_free_ptr(btnm, this);
 
+    /**
+    * Angle Labels
+    */
+    lv_style_t* style_label = new lv_style_t;
+    lv_style_copy(style_label, &lv_style_plain);
+    style_label->text.font = &lv_font_dejavu_20;
+    style_label->text.letter_space = 1;
+    style_label->text.color = LV_COLOR_WHITE;
 
-
-  /**
-  * Angle Labels
-  */
-  lv_style_t* style_label = new lv_style_t;
-  lv_style_copy(style_label, &lv_style_plain);
-  style_label->text.font = &lv_font_dejavu_20;
-  style_label->text.letter_space = 1;
-  style_label->text.color = LV_COLOR_WHITE;
-
-  for(auto &label : angleLabels) {
-    label = lv_label_create(angleContainer, NULL);
-    lv_obj_set_style(label, style_label);
+    for(auto &label : angleLabels) {
+      label = lv_label_create(angleContainer, NULL);
+      lv_obj_set_style(label, style_label);
+    }
+    calcAngleLabels();
   }
-  calcAngleLabels();
 
 
   /**
   * Action Buttons
   */
-  lv_obj_t* actionContainer = lv_obj_create(container, NULL);
-  lv_obj_set_size(actionContainer, actionContainerWidth, actionContainerHeight);
-  lv_obj_align(actionContainer, container, LV_ALIGN_IN_TOP_RIGHT, 0, 0);
-  lv_obj_set_style(actionContainer, masterStyle);
+  {
+    lv_obj_t* actionContainer = lv_obj_create(container, NULL);
+    lv_obj_set_size(actionContainer, actionContainerWidth, actionContainerHeight);
+    lv_obj_align(actionContainer, NULL, LV_ALIGN_IN_TOP_RIGHT, 0, 0);
+    lv_obj_set_style(actionContainer, masterStyle);
 
-  std::vector<const char*>* actionLabels = new std::vector<const char*>;
-  *actionLabels = {"Top", "Mid", "Cycle", "Shoot", ""};
+    std::vector<const char*>* actionLabels = new std::vector<const char*>;
+    *actionLabels = {"Top", "Mid", "Cycle", "Shoot", ""};
 
-  lv_obj_t* actionBtnm = lv_btnm_create(actionContainer, NULL);
-  lv_btnm_set_map(actionBtnm, actionLabels->data());
+    lv_obj_t* btnm = lv_btnm_create(actionContainer, NULL);
+    lv_btnm_set_map(btnm, actionLabels->data());
 
-  lv_btnm_set_style(actionBtnm, LV_BTNM_STYLE_BG, btnm_bg);
-  lv_btnm_set_style(actionBtnm, LV_BTNM_STYLE_BTN_REL, btnm_rel);
-  lv_btnm_set_style(actionBtnm, LV_BTNM_STYLE_BTN_PR, btnm_pr);
-  lv_btnm_set_style(actionBtnm, LV_BTNM_STYLE_BTN_INA, btnm_ina);
+    lv_btnm_set_style(btnm, LV_BTNM_STYLE_BG, btnm_bg);
+    lv_btnm_set_style(btnm, LV_BTNM_STYLE_BTN_REL, btnm_rel);
+    lv_btnm_set_style(btnm, LV_BTNM_STYLE_BTN_PR, btnm_pr);
+    lv_btnm_set_style(btnm, LV_BTNM_STYLE_BTN_INA, btnm_ina);
 
-  lv_obj_set_size(actionBtnm, lv_obj_get_width(actionContainer), lv_obj_get_height(actionContainer));
-  lv_btnm_set_action(actionBtnm, actionBtnAction);
-  lv_obj_set_free_ptr(actionBtnm, this);
+    lv_obj_set_size(btnm, lv_obj_get_width(actionContainer), lv_obj_get_height(actionContainer));
+    lv_btnm_set_action(btnm, actionBtnAction);
+    lv_obj_set_free_ptr(btnm, this);
+  }
 
 
-
-
+  /**
+  * Angle Dial
+  */
+  {
+    lv_obj_t* dialContainer = lv_obj_create(container, NULL);
+    lv_obj_set_size(dialContainer, dialContainerWidth, dialContainerHeight);
+    lv_obj_align(dialContainer, NULL, LV_ALIGN_IN_BOTTOM_LEFT, angleContainerWidth, 10);
+    lv_obj_set_style(dialContainer, masterStyle);
+  }
 
 
 
