aa4824eb7a6942d69ceeb0168d361e17337f0adb
theol0403 <theol0403@gmail.com>
2019-04-02 11:35:25 -0700

    rework needed

--- src/lib7842/other/controllerPrinter.cpp
+++ src/lib7842/other/controllerPrinter.cpp
@@ -5,21 +5,19 @@ namespace lib7842
 
   ControllerPrinter::ControllerPrinter(Controller* icontroller, int iline)
   : controller(icontroller), line(iline) {
-    timer.placeMark();
     controller->clear();
   }
 
   void ControllerPrinter::print(std::string str) {
     const int maxWidth = 14;
 
-    if(timer.getDtFromMark() >= 500_ms) {
-      timer.placeMark();
+    if(timer.repeat(52_ms)) {
 
-      if(str.size() < maxWidth) {
-        str.insert(str.end(), maxWidth - str.size(), ' ');
-      } else {
-        str.erase(str.begin() + maxWidth, str.end());
-      }
+      // if(str.size() < maxWidth) {
+      //   str.insert(str.end(), maxWidth - str.size(), ' ');
+      // } else {
+      //   str.erase(str.begin() + maxWidth, str.end());
+      // }
 
       controller->setText(line, 0, str);
       //std::cout << "Set Text: " << str << std::endl;
