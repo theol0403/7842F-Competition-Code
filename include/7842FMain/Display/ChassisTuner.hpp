1f4a0062aba0b9b63f81297dfa5bf9f02007dc11
theol0403 <theol0403@gmail.com>
2019-04-04 11:45:25 -0700

    chassis tuner tough

--- include/7842FMain/Display/ChassisTuner.hpp
+++ include/7842FMain/Display/ChassisTuner.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include "main.h"
-#include <map>
+#include "lib7842/odometry/controller.hpp"
 
 class ChassisTuner
 {
@@ -8,12 +8,18 @@ class ChassisTuner
 public:
 
   lv_obj_t* container = nullptr;
+  lib7842::OdomController* controller = nullptr;
 
-  ChassisTuner(lv_obj_t*);
-  ChassisTuner(lv_obj_t*, lv_color_t);
+lv_style_t containerStyle;
+
+std::vector<lv_obj_t*> tuners = {};
+using Passer = std::tuple<ChassisTuner*, IterativePosPIDController*>;
+
+  ChassisTuner(lv_obj_t*, lib7842::OdomController*);
+  ChassisTuner(lv_obj_t*, lv_color_t, lib7842::OdomController*);
   ~ChassisTuner();
 
-  void buildButton();
+  lv_obj_t* buildTuner(lv_obj_t*, std::string, IterativePosPIDController*);
 
   static lv_res_t btnAction(lv_obj_t*, const char*);
 
--- src/7842FMain/Display/ChassisTuner.cpp
+++ src/7842FMain/Display/ChassisTuner.cpp
@@ -2,10 +2,10 @@
 #include <sstream>
 #include <iomanip>
 
-ChassisTuner::ChassisTuner(lv_obj_t* parent) : ChassisTuner(parent, lv_obj_get_style(parent)->body.main_color) {}
+ChassisTuner::ChassisTuner(lv_obj_t* parent, lib7842::OdomController* icontroller) : ChassisTuner(parent, lv_obj_get_style(parent)->body.main_color, icontroller) {}
 
-ChassisTuner::ChassisTuner(lv_obj_t* parent, lv_color_t mainColor) :
-container(parent)
+ChassisTuner::ChassisTuner(lv_obj_t* parent, lv_color_t mainColor, lib7842::OdomController* icontroller) :
+container(parent), controller(icontroller)
 {
 
   /**
@@ -46,27 +46,62 @@ container(parent)
   btnm_ina->text.color = LV_COLOR_WHITE;
 
 
-    std::vector<const char*>* btnLabels = new std::vector<const char*>;
-    *btnLabels = {"Dist+", "\n", "Dist-", "\n", "Top+", "\n", "Top-", "\n", "Mid+", "\n", "Mid-", ""};
-
-    lv_obj_t* btnm = lv_btnm_create(angleContainer, NULL);
-    lv_btnm_set_map(btnm, btnLabels->data());
-
-    lv_btnm_set_style(btnm, LV_BTNM_STYLE_BG, btnm_bg);
-    lv_btnm_set_style(btnm, LV_BTNM_STYLE_BTN_REL, btnm_rel);
-    lv_btnm_set_style(btnm, LV_BTNM_STYLE_BTN_PR, btnm_pr);
-    lv_btnm_set_style(btnm, LV_BTNM_STYLE_BTN_INA, btnm_ina);
-
-    lv_obj_set_size(btnm, lv_obj_get_width(angleContainer)/3*2, lv_obj_get_height(angleContainer));
-    lv_btnm_set_action(btnm, angleBtnAction);
-    lv_obj_set_free_ptr(btnm, this);
+  lv_style_copy(&containerStyle, &lv_style_plain);
+  containerStyle.body.main_color = mainColor;
+  containerStyle.body.grad_color = mainColor;
+  containerStyle.body.border.color = LV_COLOR_WHITE;
+  containerStyle.body.border.width = 3;
+  containerStyle.body.radius = 0;
 
 
+  std::vector<const char*>* btnLabels = new std::vector<const char*>;
+  *btnLabels = {"kP+", "kP-", "kI+", "kI-", "kD+", "kD-", ""};
+
+  lv_obj_t* btnm = lv_btnm_create(container, NULL);
+  lv_btnm_set_map(btnm, btnLabels->data());
+
+  lv_btnm_set_style(btnm, LV_BTNM_STYLE_BG, btnm_bg);
+  lv_btnm_set_style(btnm, LV_BTNM_STYLE_BTN_REL, btnm_rel);
+  lv_btnm_set_style(btnm, LV_BTNM_STYLE_BTN_PR, btnm_pr);
+  lv_btnm_set_style(btnm, LV_BTNM_STYLE_BTN_INA, btnm_ina);
+
+  lv_obj_set_size(btnm, lv_obj_get_width(container), lv_obj_get_height(container)/2);
+  lv_obj_align(btnm, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, 0);
+
+  buildTuner(btnm, "Distance", controller->distancePid);
+  buildTuner(btnm, "Angle", controller->anglePid);
+  buildTuner(btnm, "Turn", controller->turnPid);
+
+  lv_obj_set_hidden(btnm, true);
+
+
+
+}
 
 
 ChassisTuner::~ChassisTuner() {
   lv_obj_del(container);
 }
+
+
+lv_obj_t* ChassisTuner::buildTuner(lv_obj_t* copy, std::string name, IterativePosPIDController* pid) {
+  lv_obj_t* tuner = lv_obj_create(container, NULL);
+  lv_obj_set_size(tuner, lv_obj_get_width(copy), lv_obj_get_height(copy));
+  lv_obj_align(tuner, NULL, LV_ALIGN_CENTER, 0, 0);
+  lv_obj_set_style(tuner, &containerStyle);
+  lv_obj_set_hidden(tuner, true);
+
+  lv_obj_t* btnm = lv_btnm_create(tuner, copy);
+  Passer* passer = new Passer(this, pid);
+
+  lv_obj_set_free_ptr(btnm, passer);
+  lv_btnm_set_action(btnm, btnAction);
+
+  return tuner;
+}
+
+
+
 
 void ChassisTuner::allignLabel(lv_obj_t* label, std::string text, double* offset) {
   int height = lv_obj_get_height(lv_obj_get_parent(label)) - 15;
