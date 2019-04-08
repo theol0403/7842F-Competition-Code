20d0ccb23afc2f6aac4b385d0944e2904669f11d
theol0403 <theol0403@gmail.com>
2019-04-07 17:45:30 -0700

    clean tracker header

--- include/lib7842/odometry/tracker.hpp
+++ include/lib7842/odometry/tracker.hpp
@@ -15,23 +15,20 @@ namespace lib7842
 
   public:
 
+    std::shared_ptr<okapi::SkidSteerModel> model;
+    const QLength m_chassisWidth;
+    const QLength m_wheelDiam;
+    const double m_degToInch;
+    std::function<void(OdomTracker*)> m_trackerFunc;
+    pros::Task m_task;
+
+    qPoint state {0_in, 0_in, 0_rad};
+
     OdomTracker (
       std::shared_ptr<okapi::SkidSteerModel>,
       QLength, QLength, double,
       std::function<void(OdomTracker*)>
     );
-
-
-    std::shared_ptr<okapi::SkidSteerModel> model;
-    const QLength m_chassisWidth;
-    const QLength m_wheelDiam;
-    const double m_degToInch;
-
-    std::function<void(OdomTracker*)> m_trackerFunc;
-
-    pros::Task m_task;
-
-    qPoint state {0_in, 0_in, 0_rad};
 
     void debug();
 
