77503438f01dbe4d0420ff562f20edb060cb5b59
theol0403 <theol0403@gmail.com>
2019-04-19 22:49:11 -0700

    smol tweak

--- utilities/visionUtility/convertVision.bat
+++ utilities/visionUtility/convertVision.bat
@@ -12,15 +12,17 @@ echo output is:
 call JREPL.BAT "\s*(?:vex::)?vision::signature\s+([a-zA-Z\d_]+)\s*\((\d)([\d\,\s\-\.]*)\);" "pros::vision_signature_s_t $1 = pros::Vision::signature_from_utility($2$3); sensor->set_signature($2, &$1);" /F input.txt /A | grep -v ", 0, 0, 0, 0, 0, 0, 3.000, 0)" | tee output.txt
 echo.
 
+rm input.txt
+
 set outputFormat=c
-set /p outputFormat=output to notepad or clipboard [n/c]?:
+set /p outputFormat=output to notepad or clipboard [n/c]? :
 
 IF "%outputFormat%"=="c" (
 cat output.txt | clip
 echo copied to clipboard!
 ) ELSE (
+echo opened notepad!
 notepad output.txt
 )
 
-rm input.txt
 rm output.txt
