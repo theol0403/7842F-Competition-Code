007ae0d2d2e6fc6b70379311a01e459ecad2edd6
theol0403 <theol0403@gmail.com>
2019-04-26 08:18:53 -0400

    close tweak

--- src/7842FMain/Auton/Close/AutonClose_NearMiddle.cpp
+++ src/7842FMain/Auton/Close/AutonClose_NearMiddle.cpp
@@ -12,7 +12,7 @@ void AutonClose_NearMiddle(void* input)
   chassis.driveDistance(-3_ft, makeSettle(4_in));
 
   AsyncAction outtake = chassis.makeAsyncAction()
-  .withTrigger(closeFlatCap, 2_ft, 30_deg)
+  .withTrigger(closeFlatCap, 3_ft, 45_deg)
   .withMakeAction(robot.intake->setState(IntakeController::outIntake););
   chassis.driveToPoint({3.3_ft, 8.5_ft}, 2, makeSettle(2_in), {outtake}); // Move to shooting position
   robot.intake->setState(IntakeController::intakeBall);
