fea912d592aac72200fb7cf6caa35959feba67b6
theol0403 <theol0403@gmail.com>
2019-05-31 22:47:08 -0700

    after worlds

--- src/7842FMain/Auton/Close/AutonClose_NearCross.cpp
+++ src/7842FMain/Auton/Close/AutonClose_NearCross.cpp
@@ -11,13 +11,15 @@ void AutonClose_NearCross(void* input)
 
   chassis.driveDistance(-3_ft, makeSettle(4_in));
 
-  robot.intake->setState(IntakeController::outIntake);
-  chassis.driveToPoint({3.3_ft, 8.5_ft}, 2, makeSettle(2_in)); // Move to shooting position
+  AsyncAction outtake = chassis.makeAsyncAction()
+  .withTrigger(closeFlatCap, 3_ft, 45_deg)
+  .withMakeAction(robot.intake->setState(IntakeController::outIntake););
+  chassis.driveToPoint({3.3_ft, 8.5_ft}, 2, makeSettle(2_in), {outtake}); // Move to shooting position
   robot.intake->setState(IntakeController::intakeBall);
 
-  chassis.turnToPoint({9_ft, 11_ft}); // turn to flag
+  chassis.turnToPoint({9_ft, 12.3_ft}); // turn to flag
 
-  robot.shooter->setDistanceToFlag(distanceToPoint({9_ft, 11_ft}));
+  robot.shooter->setDistanceToFlag(11_ft);
   robot.shooter->doMacroBlocking(ShootController::shootMacros::shootBoth);
 
 
